ffb92b3c07f81f33a91a39983d37ba9c285d895a
Bhangdongho <bhang97@naver.com>
2024-06-19 19:44:35 +0900

    인터페이스 열거형

--- app.js
+++ app.js
@@ -5,11 +5,55 @@ var age = 20;
 var gender = "male";
 var course = "Typescript";
 var completed = false;
+// 열거형 : 사용자 정의 타입
+var GenderType;
+(function (GenderType) {
+    GenderType["Male"] = "male";
+    GenderType["Female"] = "female";
+    GenderType["GenderNeutral"] = "neutral";
+})(GenderType || (GenderType = {}));
+var MyStudent = /** @class */ (function () {
+    function MyStudent() {
+        this.stdId = 91011;
+        this.stdName = "park";
+        this.age = 30;
+        this.gender = GenderType.Male;
+        this.course = "node.js";
+        this.completed = true;
+    }
+    MyStudent.prototype.setName = function (name) {
+        this.stdName = name;
+        console.log("이름 설정 : " + this.stdName);
+    };
+    return MyStudent;
+}());
+var myInstance = new MyStudent();
+myInstance.setName("엘리스");
 function getInfo(id) {
-  return null;
+    return {
+        stdId: id,
+        stdName: "lee",
+        // age: 20,
+        gender: GenderType.Female,
+        course: "javascript",
+        completed: true,
+    };
 }
+var std = {
+    stdId: 91011,
+    stdName: "park",
+    age: 30,
+    gender: GenderType.Male,
+    course: "node.js",
+    completed: true,
+};
+function setInfo(student) {
+    // console.log(student);
+}
+setInfo(std);
+// console.log(getInfo(5678));
 // 함수의 데이터 타입 명시(매개변수, 리턴타입)
 function Plus(a, b) {
-  return a + b;
+    return a + b;
 }
 // void라고 쓰면 아무것도 리턴하지 않게 된다.
