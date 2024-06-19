a101df68b78615400e7323b93c6a768a301b9bec
Bhangdongho <bhang97@naver.com>
2024-06-20 05:01:09 +0900

    11주차 2DAY

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 // 변수의 데이터 타입 명시
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var stdId = 1111;
 var stdName = "lee";
 var age = 20;
@@ -10,14 +19,14 @@ var GenderType;
 (function (GenderType) {
     GenderType["Male"] = "male";
     GenderType["Female"] = "female";
-    GenderType["GenderNeutral"] = "neutral";
+    GenderType["GenderNeutral"] = "nue";
 })(GenderType || (GenderType = {}));
 var MyStudent = /** @class */ (function () {
     function MyStudent() {
         this.stdId = 91011;
         this.stdName = "park";
         this.age = 30;
-        this.gender = GenderType.Male;
+        this.gender = "male";
         this.course = "node.js";
         this.completed = true;
     }
@@ -34,7 +43,7 @@ function getInfo(id) {
         stdId: id,
         stdName: "lee",
         // age: 20,
-        gender: GenderType.Female,
+        gender: "female",
         course: "javascript",
         completed: true,
     };
@@ -43,17 +52,63 @@ var std = {
     stdId: 91011,
     stdName: "park",
     age: 30,
-    gender: GenderType.Male,
+    gender: "male",
     course: "node.js",
     completed: true,
 };
 function setInfo(student) {
     // console.log(student);
 }
-setInfo(std);
+// setInfo(std);
 // console.log(getInfo(5678));
 // 함수의 데이터 타입 명시(매개변수, 리턴타입)
 function Plus(a, b) {
     return a + b;
 }
 // void라고 쓰면 아무것도 리턴하지 않게 된다.
+// type strOrNum = number | string;
+// let numStr: strOrNum = "100";
+// let item: number;
+// function convertToString(val: strOrNum): string {
+//   if (typeof val === "string") {
+//     item = 0;
+//   } else {
+//     item = val;
+//   }
+//   return String(val);
+// }
+// function convertToNumber(val: strOrNum): number {
+//   return Number(val);
+// }
+// console.log(convertToString(numStr));
+// console.log(convertToNumber(numStr));
+// let numbers: number[] = [1, 2, 3, 4, 5];
+// let fruits: string[] = ["apple", "banana", "orange"];
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i]);
+// }
+// 배열의 유니온 타입
+// let mixedArray: (number | stirng)[] = [1, "two", 3, "four"];
+// for (let i = 0; i < mixedArray.length; i++) {
+//   console.log(mixedArray[i]);
+// }
+// let infer = [1, 2, 3]; // 타입 추론
+// for (let i = 0; i < infer.length; i++) {
+//   console.log(infer[i]);
+// }
+// let readOnlyArray: ReadonlyArray<number> = [1, 2, 3];
+// 튜플 : 타입의 순서가 정해져 있다.
+// let greeting: [number, string, boolean] = [1, "hello", true];
+// for (let i = 0; i < greeting.length; i++) {
+//   console.log(greeting[i]);
+// }
+// Spread 연산자
+var firstArray = [1, 2, 3];
+var secondArray = [4, 5, 6];
+var combineArray = __spreadArray(__spreadArray([], firstArray, true), secondArray, true);
+for (var i = 0; i < combineArray.length; i++) {
+    console.log(combineArray[i]);
+}
--- app.ts
+++ app.ts
@@ -11,14 +11,14 @@ let completed: boolean = false;
 enum GenderType {
   Male = "male",
   Female = "female",
-  GenderNeutral = "neutral",
+  GenderNeutral = "nue",
 }
 
 interface Student {
   stdId: number;
   stdName?: string;
   age?: number;
-  gender?: GenderType;
+  gender?: "male" | "female";
   course?: string;
   completed?: boolean;
   // setName(name: string): void;
@@ -30,7 +30,7 @@ class MyStudent implements Student {
   stdId = 91011;
   stdName = "park";
   age = 30;
-  gender = GenderType.Male;
+  gender: "male" | "female" = "male";
   course = "node.js";
   completed = true;
 
@@ -48,7 +48,7 @@ function getInfo(id: number): Student {
     stdId: id,
     stdName: "lee",
     // age: 20,
-    gender: GenderType.Female,
+    gender: "female",
     course: "javascript",
     completed: true,
   };
@@ -58,7 +58,7 @@ let std = {
   stdId: 91011,
   stdName: "park",
   age: 30,
-  gender: GenderType.Male,
+  gender: "male",
   course: "node.js",
   completed: true,
 };
@@ -67,7 +67,7 @@ function setInfo(student: Student): void {
   // console.log(student);
 }
 
-setInfo(std);
+// setInfo(std);
 
 // console.log(getInfo(5678));
 
@@ -77,3 +77,69 @@ function Plus(a: number, b: number): number {
 }
 
 // void라고 쓰면 아무것도 리턴하지 않게 된다.
+
+// type strOrNum = number | string;
+
+// let numStr: strOrNum = "100";
+// let item: number;
+
+// function convertToString(val: strOrNum): string {
+//   if (typeof val === "string") {
+//     item = 0;
+//   } else {
+//     item = val;
+//   }
+
+//   return String(val);
+// }
+
+// function convertToNumber(val: strOrNum): number {
+//   return Number(val);
+// }
+
+// console.log(convertToString(numStr));
+// console.log(convertToNumber(numStr));
+
+// let numbers: number[] = [1, 2, 3, 4, 5];
+
+// let fruits: string[] = ["apple", "banana", "orange"];
+
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i]);
+// }
+
+// 배열의 유니온 타입
+// let mixedArray: (number | stirng)[] = [1, "two", 3, "four"];
+
+// for (let i = 0; i < mixedArray.length; i++) {
+//   console.log(mixedArray[i]);
+// }
+
+// let infer = [1, 2, 3]; // 타입 추론
+
+// for (let i = 0; i < infer.length; i++) {
+//   console.log(infer[i]);
+// }
+
+// let readOnlyArray: ReadonlyArray<number> = [1, 2, 3];
+
+// 튜플 : 타입의 순서가 정해져 있다.
+// let greeting: [number, string, boolean] = [1, "hello", true];
+
+// for (let i = 0; i < greeting.length; i++) {
+//   console.log(greeting[i]);
+// }
+
+// Spread 연산자
+let firstArray = [1, 2, 3];
+let secondArray = [4, 5, 6];
+
+let combineArray = [...firstArray, ...secondArray];
+
+for (let i = 0; i < combineArray.length; i++) {
+  console.log(combineArray[i]);
+}
